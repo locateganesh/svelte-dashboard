16f88e402d706578c79a8f43a317d4b4bf0867f5
Ganesh Yadav <locateganesh@gmail.com>
2024-04-12 10:55:05 +0530

    Update BarStacked.svelte

--- src/_components/BarStacked.svelte
+++ src/_components/BarStacked.svelte
@@ -3,7 +3,7 @@
   Generates an SVG stacked bar chart and sets the color via an ordinal scale in `zScale`.
  -->
 <script>
-  import { getContext } from 'svelte';
+  import { getContext, createEventDispatcher } from 'svelte';
 
   const { data, xGet, yGet, zGet, yScale } = getContext('LayerCake');
 
@@ -15,22 +15,40 @@
 		return xVals[1] - xVals[0];
   };
 
-  // console.log("separator", separator);
+  const dispatch = createEventDispatcher();
+  function handleMousemove() {
+    return function handleMousemoveFn(e) {
+      raise(this);
+      // When the element gets raised, it flashes 0,0 for a second so skip that
+      if (e.layerX !== 0 && e.layerY !== 0) {
+        dispatch("mousemove", { e });
+      }
+    };
+  }
+
+  // console.log("$data", $data);
 </script>
 
-<g class="bar-group">
+<g 
+  class="bar-group"
+  on:mouseout={() => dispatch("mouseout")}
+  on:blur={() => dispatch("mouseout")}
+  >
   {#each $data as series}
     {#each series as d, i}
       <rect
         class='group-rect'
         data-id="{i}"
+        data-d={d}
         x="{$xGet(d)[0]}"
         y="{$yGet(d)}"
         height={$yScale.bandwidth() / 2}
         width="{columnWidth(d)}"
         fill={$zGet(series)}
+        on:mouseover={(e) => dispatch("mousemove", { e, props: d })}
+        on:focus={(e) => dispatch("mousemove", { e, props: d })}
+        on:mousemove={(e) => handleMousemove(e, d)}
       >
-      <text>ahhd</text>
     </rect>
       {#if separator}
         <!-- <line x1={columnWidth(d) / 2 } y1="50" x2={columnWidth(d)} y2="50" stroke="#aeaeae" stroke-width="1" /> -->
